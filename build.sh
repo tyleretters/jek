88fd83d2c4716f389c3c52bdc43159ab570a9cf9
Tyler Etters <tyler@etters.co>
2024-10-31 08:33:39 -0700

    try --trace

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ bundle install
 sass assets/stylesheets/style.scss:assets/stylesheets/style.css
 echo "hash: $(git rev-parse HEAD)" > _data/build.yml
 echo "time: $(date -u +"%Y-%m-%dT%H:%M:%SZ")" >> _data/build.yml
-jekyll build
+jekyll build --trace
