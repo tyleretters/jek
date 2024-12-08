397d3834d86a341b22a67b498eae053529ffc321
Tyler Etters <tyler@etters.co>
2024-12-08 09:58:03 -0800

    short git rev parse

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 bundle install
 sass assets/stylesheets/style.scss:assets/stylesheets/style.css
-echo "hash: $(git rev-parse HEAD)" > _data/build.yml
+echo "hash: $(git rev-parse --short HEAD)" > _data/build.yml
 echo "time: $(date -u +"%Y-%m-%dT%H:%M:%SZ")" >> _data/build.yml
-jekyll build
+bundle exec jekyll build
